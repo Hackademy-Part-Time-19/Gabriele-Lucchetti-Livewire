795d7022aaa24da7b5fb869094f14327a3c41c24
GabLux <gabrielelucchetti@live.it>
2024-02-29 20:57:44 +0100

    navbar modificata

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -15,9 +15,7 @@
           <li class="nav-item">
             <a class="nav-link" href="{{route('posts.index')}}">Posts</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
+          
         </ul>
       </div>
     </div>
